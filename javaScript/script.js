442b656a1538a9fc8616eceb2f47fade85f87f93
RAFAEL ANTONIO BRITO MOREIRA <106538987+RafaelMoreiraDev@users.noreply.github.com>
2023-10-28 18:42:00 -0300

    subindo atualizaçoes do css

--- javaScript/script.js
+++ javaScript/script.js
@@ -15,3 +15,35 @@ proximoButton.forEach((button, index) => {
     categoriaUl[index].scrollLeft += 350;
   });
 });
+
+
+
+
+
+
+/* Templetes */
+
+
+loja.templates = {
+  sliderHome:
+            `<div class="bg-1">
+            <h2 class="produto-blusa">\${categoria}</h2>
+            <section class="categoria-blusa categoria">
+              
+              <ul >
+                <li><a href="./html/categoria.html"><img src="./img/blusa1.jpg" alt="blusa"></a></li>
+                <li><a href="./html/categoria.html"><img src="./img/blusa2.jpg" alt="blusa"></a></li>
+                <li><a href="./html/categoria.html"><img src="./img/blusa3.jpg" alt="blusa"></a></li>
+                <li><a href="./html/categoria.html"><img src="./img/blusa4.jpg" alt="blusa"></a></li>
+                <li><a href="./html/categoria.html"><img src="./img/blusa2.jpg" alt="blusa"></a></li>
+                <li><a href="./html/categoria.html"><img src="./img/blusa1.jpg" alt="blusa"></a></li>
+              </ul>
+              
+            </section>
+            <div class="btn">
+              <a  class="anterior botao-comprar"> <</a>
+              <a class="proximo botao-comprar">></a>
+            </div>
+          </div>`
+
+}
