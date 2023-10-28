9ea6c52c2f6e839c9ef0ac3bb7bb98f78a7671c3
RAFAEL ANTONIO BRITO MOREIRA <106538987+RafaelMoreiraDev@users.noreply.github.com>
2023-10-28 00:39:06 -0300

    subindo função dos slider e obj dos produtos

---
+++ javaScript/dados.js
@@ -0,0 +1,82 @@
+var ROUPAS = {
+    "blusas":[
+        {
+            "id": "01",
+            "nome": "Blusa Branca Florida",
+            "img": "./img/blusa1.jpg",
+            "tamanho": "P",
+            "cor":  "Branca",
+            "valor": 47.99 ,
+            "categoria": "Blusas"
+        },
+        {
+            "id": "02",
+            "nome": "Blusa de Ceda",
+            "img": "./img/blusa2.jpg",
+            "tamanho": "M",
+            "cor":  "Azul",
+            "valor": 79 ,
+            "categoria": "Blusas"
+        },
+        {
+            "id": "03",
+            "nome": "Blusa Verão Florida",
+            "img": "./img/blusa3.jpg",
+            "tamanho": "G",
+            "cor":  "Branca",
+            "valor": 54.99 ,
+            "categoria": "Blusas"
+        },
+        {
+            "id": "04",
+            "nome": "Blusa Casual",
+            "img": "./img/blusa4.jpg",
+            "tamanho": "M",
+            "cor":  "Branca",
+            "valor": 68.90 ,
+            "categoria": "Blusas"
+        }
+        
+    ],
+    "calças":[
+        {
+            "id": "05",
+            "nome": "Calça Jeans Verão",
+            "img": "./img/calca1.jpg",
+            "tamanho": "38",
+            "cor":  "Azul",
+            "valor": 107.99 ,
+            "categoria": "Calças"
+        },
+        {
+            "id": "06",
+            "nome": "Calça Primavera",
+            "img": "./img/calca2.jpg",
+            "tamanho": "36",
+            "cor":  "Amarela",
+            "valor": 112.99 ,
+            "categoria": "Calças"
+        },
+        {
+            "id": "07",
+            "nome": "Calça Jeans Outono",
+            "img": "./img/calca3.jpg",
+            "tamanho": "36",
+            "cor":  "Preta",
+            "valor": 125.99 ,
+            "categoria": "Calças"
+        },
+        {
+            "id": "07",
+            "nome": "Calça Moda Verão",
+            "img": "./img/calca4.jpg",
+            "tamanho": "42",
+            "cor":  "Azul",
+            "valor": 102.99 ,
+            "categoria": "Calças"
+        },  
+    ]
+
+
+
+}
--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,17 @@
 
+/*animação dos slider*/
+const categoriaUl = document.querySelectorAll(".categoria ul");
+const anteriorButton = document.querySelectorAll(".anterior");
+const proximoButton = document.querySelectorAll(".proximo");
 
+anteriorButton.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    categoriaUl[index].scrollLeft -= 350;
+  });
+});
 
-
+proximoButton.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    categoriaUl[index].scrollLeft += 350;
+  });
+});
