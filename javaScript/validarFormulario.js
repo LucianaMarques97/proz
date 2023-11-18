ae78087936f2d1f5daaae5e32fb62b737c84bd08
RAFAEL ANTONIO BRITO MOREIRA <106538987+RafaelMoreiraDev@users.noreply.github.com>
2023-11-18 00:51:04 -0300

    usando api dos correios pra endereço

--- javaScript/validarFormulario.js
+++ javaScript/validarFormulario.js
@@ -66,6 +66,56 @@ function formaPagamentoEscolhida(formaPg) {
 
 
 
+
+
+
+
+// inicio dos testes dos correios
+
+function validarEndereco() {
+  // Aqui você pode adicionar lógica de validação adicional antes de enviar o formulário
+  return true;
+}
+
+document.getElementById("txtEndereco1").addEventListener("blur", function () {
+  var cepValor = this.value.trim().replace(/[^0-9]/g, '');
+
+  if (cepValor !== '') {
+      var validacep = /^[0-9]{8}$/;
+      
+
+      if (validacep.test(cepValor)) {
+          var xhr = new XMLHttpRequest();
+          xhr.open("GET", "https://viacep.com.br/ws/" + cepValor + "/json/");
+          xhr.onreadystatechange = function () {
+              if (xhr.readyState === 4 && xhr.status === 200) {
+                  var dados = JSON.parse(xhr.responseText);
+                  if (!("erro" in dados)) {
+                      document.getElementById("txtRua").value = dados.logradouro;
+                      document.getElementById("txtBairro").value = dados.bairro;
+                      document.getElementById("txtCidade").value = dados.localidade;
+                      document.getElementById("ddlUF").value = dados.uf
+                      document.getElementById("txtNumero").focus();
+                  } else {
+                      alert("CEP não encontrado. Preencha as informações manualmente.");
+                      document.getElementById("txtEndereco").focus();
+                  }
+              }
+          };
+          xhr.send();
+      } else {
+          alert("Formato inválido para CEP.");
+      }
+  } else {
+      alert("O campo CEP não pode estar vazio.");
+  }
+});
+//fim dos testes de endereço
+
+
+
+
+
 // Renderizar endereço
 const endereco = document.getElementById('endereco');
 function renderizarFormEndereco() {
@@ -79,7 +129,7 @@ function renderizarFormEndereco() {
 // Validar campos do formulário de endereço
 function validarEndereco() {
   // Obter os valores dos campos de entrada
-  let endereco = document.getElementById('txtEndereco').value;
+  let endereco = document.getElementById('txtEndereco1').value;
   let bairro = document.getElementById('txtBairro').value;
   let numero = document.getElementById('txtNumero').value;
   let cidade = document.getElementById('txtCidade').value;
